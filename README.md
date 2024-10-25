bff957e51fe0764ed388784ea667041001dfb272
Garrett Johnson <pingujohnson@gmail.com>
2024-10-25 15:39:24 -0400

    Trying numpy

---
+++ README.md
@@ -0,0 +1,15 @@
+# Match Titles to Files
+
+A program that takes a csv of file names and attempts to match them to files
+...
+It may also sort said files later, or that may be a separate script later
+
+### Linux
+
+#### Initial Run
+
+- cd /your/folder
+- python3 -m venv env
+- source env/bin/activate
+- pip install -r requirements.txt
+- python main.py
---
+++ main.py
@@ -0,0 +1,177 @@
+# Header Comment
+# Project: [Garrett's Split Tracker] [https://github.com/GreenBeanio/Match-Titles-to-Files]
+# Copyright: Copyright (c) [2024]-[2024] [Match Titles to Files] Contributors
+# Version: [0.1]
+# Status: [Development]
+# License(s): [MIT]
+# Author(s): [Garrett Johnson (GreenBeanio) - https://github.com/greenbeanio]
+# Maintainer: [Garrett Johnson (GreenBeanio) - https://github.com/greenbeanio]
+# Project Description: [This project is used to try to match titles to corresponding files. ]
+# File Description: This is the main file for the project.
+
+# Imports
+import pandas
+import thefuzz
+import pathlib
+import logging
+import sys
+import argparse
+import numpy
+
+
+# Class to hold the path settings
+class UserPaths:
+    def __init__(self, input_csv, file_directory, output_csv) -> None:
+        self.input_csv = input_csv
+        self.file_directory = file_directory
+        self.output_csv = output_csv
+
+
+# Function to check if a path exists
+def checkPath(test_path: pathlib.Path) -> bool:
+    if pathlib.Path.exists(test_path):
+        return True
+    else:
+        return False
+
+
+# Function to get the arguments
+def checkArguments() -> UserPaths:
+    # Get the current directory
+    cwd = pathlib.Path().resolve()
+    # Getting path options
+    if cli_args.csv_path is None:
+        input_csv = pathlib.Path.joinpath(cwd, "titles.csv")
+    else:
+        input_csv = cli_args.csv_path
+    if cli_args.files_path is None:
+        files_path = pathlib.Path.joinpath(cwd, "files")
+    else:
+        files_path = cli_args.files_path
+    if cli_args.output_path is None:
+        output_csv = pathlib.Path.joinpath(cwd, "matched.csv")
+    else:
+        output_csv = cli_args.output_path
+    # Checking the the input csv exists and that the file directory exists
+    good_paths = True
+    if not checkPath(input_csv):
+        good_paths = False
+        logger.error(f'The input file at "{input_csv}" does not exist')
+    if not checkPath(files_path):
+        good_paths = False
+        logger.error(f'The input directory at "{files_path}" does not exist')
+    elif not any(files_path.iterdir()):
+        # Check that the directory isn't empty
+        good_paths = False
+        logger.error(f'The input directory at "{files_path}" is empty')
+    # Checking that the output file is in a directory that exists
+    if not checkPath(output_csv.parent):
+        good_paths = False
+        logger.error(f'The output directory at "{output_csv.parent}" does not exist')
+    # Close if there was an invalid path
+    if not good_paths:
+        logger.critical("Fix the errors in your files")
+        sys.exit()
+    # Create the class to store the files
+    return UserPaths(input_csv, files_path, output_csv)
+
+
+# Function to read the csv
+def readCsv(csv_path: pathlib.Path) -> pandas.DataFrame:
+    # Try to open the csv file
+    try:
+        # try to read the csv
+        df = pandas.read_csv(csv_path, header=None, usecols=[0, 1])
+        return df
+    except:
+        logger.error(f'The file at "{csv_path}" couldn\'t be read')
+        sys.exit()
+
+
+# Function to get the file names
+def getFiles(file_path: pathlib.Path) -> pandas.DataFrame:
+    # Creating an empty numpy array
+    temp_array = numpy.empty((0, 3))
+    # Iterate through every item in the path
+    for item in file_path.iterdir():
+        # Add the file to a numpy array
+        if item.is_file():
+            # temp_array = numpy.concatenate(
+            #     (temp_array, [[item, item.stem, True]]), axis=0
+            # )
+            temp_array = numpy.append(temp_array, [[item, item.stem, True]], axis=0)
+        else:
+            # temp_array = numpy.concatenate(
+            #     (temp_array, [[item, item.stem, False]]), axis=0
+            # )
+            temp_array = numpy.concatenate(
+                (temp_array, [[item, item.stem, False]]), axis=0
+            )
+    print(temp_array)
+    print(temp_array.shape[0])
+    print(temp_array.shape[1])
+    print(temp_array[0:])
+    temp_array = temp_array.reshape(11, 3)
+    # # Create a data frame
+    # temp_df = pandas.DataFrame(
+    #     data=temp_array[0:],
+    #     # columns=[f"Yummy_{i}" for i in range(0, temp_array.shape[0])],
+    #     # index=[i for i in range(0, temp_array.shape[1])],
+    # )
+    # print(temp_df)
+
+
+# Creating a logger
+# Create a logger
+logger = logging.getLogger("Title Matcher")
+logger.setLevel(logging.INFO)
+
+# Creating the cli arguments
+parser = argparse.ArgumentParser(
+    prog="Match Titles to Files",
+    description="Attempts to match titles to files.",
+    epilog="Garrett Johnson (GreenBeanio) - https://github.com/greenbeanio",
+)
+parser.add_argument(
+    "-csv",
+    "--csv_path",
+    help="Path to the csv file. Default: ./titles.csv",
+    type=pathlib.Path,
+)
+parser.add_argument(
+    "-files",
+    "--files_path",
+    help="Path to the directory with the files to attempt to match the titles to. Default: ./files",
+    type=pathlib.Path,
+)
+parser.add_argument(
+    "-o",
+    "--output_path",
+    help="Path to where you want to new (or overwritten) csv to go. Default: ./matched.csv",
+    type=pathlib.Path,
+)
+cli_args = parser.parse_args()
+
+# Get the user arguments
+user_args = checkArguments()
+
+# Getting the input csv data in a data frame
+input_df = readCsv(user_args.input_csv)
+
+# Add column names
+input_df.columns = ["Title", "Path"]
+
+# Creating an index column (for recombining at the end)
+input_df["Index"] = input_df.index
+
+# Splitting the data frame into 2 data frames (Might need to make these as deep copies later, believe these still reference the same data)
+search_df = input_df[input_df["Path"].isnull()]  # .copy()
+found_df = input_df[input_df["Path"].notnull()]  # .copy()
+
+# Reading all the files in the file directory
+file_titles = getFiles(user_args.file_directory)
+
+
+# Footer Comment
+# History of Contributions:
+# [2024-2024] - [Garrett Johnson (GreenBeanio) - https://github.com/greenbeanio] - [The entire document]
